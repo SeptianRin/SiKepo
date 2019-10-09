fa2422b98a2c29ea353a5ce1d7026b5401d8264f
septianrin <septianrinaldi98@gmail.com>
2019-10-09 15:31:49 +0700

    add clickable recyclerview

---
+++ app/src/main/java/com/klepon/kkn/isikuliner/kuliner2.java
@@ -0,0 +1,16 @@
+package com.klepon.kkn.isikuliner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.klepon.kkn.R;
+
+public class kuliner2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kuliner2);
+    }
+}
---
+++ app/src/main/java/com/klepon/kkn/isiumkm/umkm4.java
@@ -0,0 +1,16 @@
+package com.klepon.kkn.isiumkm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.klepon.kkn.R;
+
+public class umkm4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.umkm4);
+    }
+}
