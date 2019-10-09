16ad64c5f5dc8197935856ef66f0139af2fab0c6
septianrin <septianrinaldi98@gmail.com>
2019-10-09 17:02:27 +0700

    add versi 0.3

---
+++ app/src/main/java/com/klepon/kkn/isiwisata/wisata6.java
@@ -0,0 +1,16 @@
+package com.klepon.kkn.isiwisata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.klepon.kkn.R;
+
+public class wisata6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wisata6);
+    }
+}
